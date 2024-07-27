4f989a113d515a7250fbb5c2e96c3637e1364231
Yehonatan Mizrachi <ymizrachi5@gmail.com>
2024-07-27 15:36:15 +0300

    Its working! (Well, I hope so)

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+import io
+
 import cv2
 import imagehash
-from PIL import Image
+from PIL import Image, ImageChops
 
-FRAME_HASHES_WINDOW_SIZE = 30
-SIMILAR_FRAMES_LOOP_THRESHOLD = 5
+from consts import IMAGE_HASH_SIZE, FRAME_SAMPLE_INTERVAL, HASH_LIST_WINDOW_SIZE_IN_SECONDS, \
+    LOOP_CHECK_INTERVAL_IN_SECONDS, LOOP_MIN_OCCURRENCES, LOOP_MIN_DURATION_IN_SECONDS, LOOP_DIFF_MARGIN_ERROR, \
+    INPUT_FPS
+from utils import has_looped_item
 
 
 def main() -> None:
-    cap = cv2.VideoCapture('./part_1.mp4')
+    cap = cv2.VideoCapture('./looped-video.mp4')
+    # cap = cv2.VideoCapture('rtsp://192.168.132.115:8080/h264_ulaw.sdp')
 
-    frames_hashes: dict[str, int] = {}
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        return
 
-    last_frame_hash = ''
+    fps = INPUT_FPS if INPUT_FPS else cap.get(cv2.CAP_PROP_FPS)
+    print(f"Frame rate: {fps}")
+
+    effective_fps = fps / FRAME_SAMPLE_INTERVAL
+    hash_list_size = effective_fps * HASH_LIST_WINDOW_SIZE_IN_SECONDS
+    loop_check_interval = effective_fps * LOOP_CHECK_INTERVAL_IN_SECONDS
+    loop_min_size = effective_fps * LOOP_MIN_DURATION_IN_SECONDS
+
     frame_count = 0
-    count_from_last_new_frame = 0
+    frames_diff_hashes: list[str] = []
+    previous_frame_pil: Image = None
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -21,34 +36,31 @@ def main() -> None:
         if not ret:
             break
 
-        frame_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        frame_hash = str(imagehash.average_hash(frame_pil))
+        frame_count += 1
 
-        if frame_hash == last_frame_hash:
+        if frame_count % FRAME_SAMPLE_INTERVAL:
             continue
 
-        if frame_hash in frames_hashes:
-            hash_count = frames_hashes.pop(frame_hash)
-            frames_hashes[frame_hash] = hash_count + 1
-            count_from_last_new_frame += 1
-            print(f'Similar frame: {frame_hash}')
-        else:
-            frames_hashes[frame_hash] = 1
-            count_from_last_new_frame = 0
+        frame_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        img_bytes = io.BytesIO()
+        frame_pil.save(img_bytes, format='PNG')
 
-        print(frames_hashes)
+        if previous_frame_pil:
+            diff = ImageChops.difference(frame_pil, previous_frame_pil)
+            diff = diff.convert('L')  # Convert the difference to grayscale
+            # diff.save(f'./frames/diff_{frame_count}.png')
 
-        if frame_count % FRAME_HASHES_WINDOW_SIZE == 0 and frame_count > 0:
-            oldest_hash = next(iter(frames_hashes))
-            frames_hashes.pop(oldest_hash)
+            diff_hash = str(imagehash.average_hash(diff, hash_size=IMAGE_HASH_SIZE))
+            frames_diff_hashes.append(diff_hash)
 
-        if count_from_last_new_frame > 10:
-            if all(map(lambda x: x > 5, frames_hashes.values())):
-                print('We\'re in a loop!')
-                return
+        if frame_count % loop_check_interval == 0:
+            has_loop = has_looped_item(frames_diff_hashes, loop_min_size, LOOP_MIN_OCCURRENCES, LOOP_DIFF_MARGIN_ERROR)
+            print(f'Has loop: {has_loop}')
 
-        last_frame_hash = frame_hash
-        frame_count += 1
+        previous_frame_pil = frame_pil
+
+        if len(frames_diff_hashes) > hash_list_size:
+            frames_diff_hashes.pop(0)
 
 
 if __name__ == '__main__':
